eee652bb29eab54f297db41a47197af6ebcfe74c
Dario Petrillo <dario.pk1@gmail.com>
2018-11-03 16:48:24 +0000

    State

--- server/state.py
+++ server/state.py
@@ -1,11 +1,25 @@
-from typing import List, Dict
+#!/usr/bin/env python3
 
+from typing import List, Dict
+from json import json
 
 class State:
     def __init__(self, config: str):
         self.known_ips: List[str] = list()
         self.mode: Dict[str, str] = dict()
-        # TODO read config file
+        self.read_config()
 
     def get_config(self, ip: str):
         raise NotImplementedError()
+    
+    def read_config(self):
+        with open('config.json') as f:
+            data = json.load(f)
+
+            # Sanity check
+            for mode in data:
+                if 'contestant' not in data[mode] or 'worker' not in data[mode]:
+                    if 'any' not in data[mode]:
+                        raise Exception('Malformed config file')
+
+            self.config = d
